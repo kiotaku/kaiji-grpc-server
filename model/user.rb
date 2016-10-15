6a910bd18740b33d9fdf07c062bf5c293cb2b2c6
kiotaku <kkutalol@gmail.com>
2016-10-15 14:42:18 +0900

    メソッドをクラスメソッドに変更およびポイント増加のメソッドの追加

--- model/user.rb
+++ model/user.rb
@@ -1,12 +1,18 @@
 class User < ActiveRecord::Base
-  belongs_to :blackjack_room, foreign_key: user_id
+  class << self
+    def add(id, params = {})
+      user = User.new params.merge(id: id)
+      user.save
+    end
 
-  def self.add(id, params = {})
-    user = User.new params.merge(id: id)
-    user.save
-  end
+    def modify(id, params = {})
+      User.find(id).update(params)
+    end
 
-  def self.modify(id, params = {})
-    User.find(id).update(params)
+    def add_point(id, points)
+      user = User.find(id)
+      points_balance = user.points
+      user.update(points: points_balance + points)
+    end
   end
 end
