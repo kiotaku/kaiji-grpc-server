b4874e215a4fff8498e6f63637954d03a6fc46d2
kiotaku <kkutalol@gmail.com>
2016-10-15 18:18:30 +0900

    Dockerfileの名前を変更

--- build-env.sh
+++ build-env.sh
@@ -1,2 +1,2 @@
-docker build -t grpc-base ./build-env
-docker run -v /c/Users/takumi/Documents/3J/kaiji-grpc-server/:/host/ grpc-base
+docker build -t protoc-compile -f ./build-env/Dockerfile-protoc ./build-env
+docker run -v /c/Users/takumi/Documents/3J/kaiji-grpc-server/:/host/ protoc-compile
