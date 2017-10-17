b2dfb25778aadb7a547a0736777893654cf54b4c
kiotaku <kkutalol@gmail.com>
2017-10-17 10:05:12 +0900

    fix typo

--- routes/kaiji_routes.rb
+++ routes/kaiji_routes.rb
@@ -29,7 +29,7 @@ class KaijiRoutes < Sinatra::Base
 
   post('/add_user') do
     result = User.add(params[:userId],
-                      params[:autoAssignId]
+                      params[:autoAssignId],
                       name: params[:name],
                       is_available: params[:isAvailable],
                       is_anonymous: params[:isAnonymous])
